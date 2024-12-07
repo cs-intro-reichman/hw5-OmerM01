aaafede0a4b7f3bb6b1b7510f1bfbc4d602d9c7a
OmerM01 <omerwork00@gmail.com>
2024-12-07 16:33:03 +0200

    HW_10

--- Scrabble.java
+++ Scrabble.java
@@ -48,8 +48,8 @@ public class Scrabble {
 
 	// Checks if the given word is in the dictionary.
 	public static boolean isWordInDictionary(String word) {
-		for(int i = 0; i < NUM_OF_WORDS ; i++){
-			if(DICTIONARY[i].equals(word)){
+		for(int i = 0; i < DICTIONARY.length; i++){
+			if(word.equals(DICTIONARY[i])){
 				return true;
 			}
 		}
