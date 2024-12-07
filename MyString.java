094d9f645a0128a184be385fdd42f16aabcb246f
OmerM01 <omerwork00@gmail.com>
2024-12-07 13:47:59 +0200

    HW_03

--- MyString.java
+++ MyString.java
@@ -44,8 +44,11 @@ public class MyString {
      * @return true is str1 is a subset of str2, false otherwise
      */
     public static boolean subsetOf(String str1, String str2) {
+        if(str1 == ""){
+            return true;
+        }
         for(int i = 0; i < str1.length(); i++){
-            if(countChar(str2, str1.charAt(i)) == 0){
+            if(countChar(str2, str1.charAt(i)) != 1){
                 return false;
             }
         }
